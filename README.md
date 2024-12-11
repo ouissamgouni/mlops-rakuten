0abca2cdb76fab96a441b02bc2688873ce21618c
Ouissam Gouni <g.ouissam@gmail.com>
2024-12-11 14:33:25 +0100

    helm chart & monitoring

--- README.md
+++ README.md
@@ -6,53 +6,56 @@ Short description of the project
 
 ![alt text](docs/mlops_design.jpg)
 
-## Installation
+## Prerequisites
 
 ### Infra
-
-Prerequisites:
-
+Install:
 * [Docker](https://docs.docker.com/get-docker/) and docker-compose
 * [Minikube](https://minikube.sigs.k8s.io/docs/start/) or local Kubernetes cluster
 * [Helm](https://helm.sh/docs/intro/install/)
 ``` bash
+# Launch your local Kubernetes cluster
 minikube start
 ```
-
+Then :
 * [Postgresql](https://phoenixnap.com/kb/postgresql-kubernetes)
 * [Mlflow](https://github.com/bitnami/charts/tree/main/bitnami/mlflow)
-* [Monitoring stack (Prometheus & Grafana)](https://medium.com/@brightband/deploying-prometheus-operator-to-a-kubernetes-cluster-c2378038c79b)
-* [Private docker registry](https://hub.docker.com/r/ogouni604/mlops-rakuten) 
+* [Prometheus & Grafana](https://medium.com/@brightband/deploying-prometheus-operator-to-a-kubernetes-cluster-c2378038c79b)
 * [Minio](https://medium.com/@kapincev/easy-guide-setting-up-minio-with-microk8s-kubernetes-321048d901ac)
 
 * [Flyte](https://github.com/davidmirror-ops/flyte-the-hard-way/blob/main/docs/on-premises/single-node/002-single-node-onprem-install.md)
 ``` bash
+# Init Flyte project
 flytectl create project      
     --id "rakuten" \
     --description "Rakuten product category predictor" \
     --name "Rakuten"
 ```
+* Docker login to [private Docker registry](https://hub.docker.com/r/ogouni604/mlops-rakuten) 
+### Tooling
+* [Taskfile](https://taskfile.dev/installation/)
 
 ## Usage
 
 ### Run the ML workflow
 
 ``` bash
-pyflyte run --remote {path_to_workflow.py} {workflow_name}
+task run-workflow
 ```
 
-### Build and deploy the ML service
+### Download a Model version
 ``` bash
-mlflow artifacts download --artifact-uri "models:/rakuten-pcat/{version}"  --dst-path model
-
-docker build -t ogouni604/mlops-rakuten:{version} .
-
-docker push ogouni604/mlops-rakuten:{version}
-
-kubectl apply -f k8s-dep.yaml
+task pull-model {version}
+```
+### Build and deploy the inference service
+``` bash
+task build push deploy
 ```
 
-## How it looks like
+### Update monitoring
+``` bash
+task update-monitoring
+```
 
 
 ## References
--- app/service-api/src/main.py
+++ app/service-api/src/main.py
@@ -1,58 +1,122 @@
 from contextlib import asynccontextmanager
+from evidently import ColumnMapping
 from fastapi import FastAPI
 import mlflow
-from fastapi.middleware.cors import CORSMiddleware
 from prometheus_fastapi_instrumentator import Instrumentator
 from pydantic import BaseModel
-from typing import List
+from typing import Callable, List
+from prometheus_fastapi_instrumentator.metrics import Info
+from prometheus_client import Counter, Gauge
+from evidently.report import Report
+from evidently.metric_preset import DataDriftPreset, ClassificationPreset
+from sklearn import datasets
+from fastapi import FastAPI
+from datetime import datetime
+from contextlib import asynccontextmanager
+from apscheduler.schedulers.background import BackgroundScheduler  # runs tasks in the background
+from apscheduler.triggers.cron import CronTrigger  # allows us to specify a recurring time for execution
 
 ml_models = {}
+
+gauge_acc = Gauge('accuracy', 'accuracy')
+gauge_precision = Gauge('precision', 'precision')
+gauge_recall = Gauge('recall', 'recall')
+gauge_f1 = Gauge('f1_score', 'f1-score')
+
+def evaluate():
+    print(f"Evaluating the model at {datetime.now()}")
+    model =  get_model()
+
+    iris_data = datasets.load_iris(as_frame=True)
+    iris_frame = iris_data.frame
+    iris_ref = iris_frame.sample(n=150, replace=False)
+    iris_cur = iris_frame.sample(n=150, replace=False)
+    #Reference and current data for Multiclass classification, option 1
+    iris_ref['prediction'] = model.predict(iris_ref[iris_data.feature_names])
+    iris_cur['prediction'] = model.predict(iris_cur[iris_data.feature_names])
+
+    data_drift_report = Report(metrics=[
+        ClassificationPreset(),
+        ])
+
+    column_mapping = ColumnMapping()
+
+    column_mapping.target = 'target'
+    column_mapping.prediction = 'prediction'
+    column_mapping.target_names = ['Setosa', 'Versicolour', 'Virginica']
+    data_drift_report.run(current_data=iris_cur, reference_data=iris_ref, column_mapping=column_mapping)
+    data_drift_report.save_json('report.json')
+
+    metrics = {}
+    report_dict = data_drift_report.as_dict()
+    metrics["accuracy"] = report_dict["metrics"][0]["result"]["current"]["accuracy"]
+    metrics["precision"] = report_dict["metrics"][0]["result"]["current"]["precision"]
+    metrics["recall"] = report_dict["metrics"][0]["result"]["current"]["recall"]
+    metrics["f1-score"] = report_dict["metrics"][0]["result"]["current"]["f1"]
+
+    gauge_acc.set(metrics["accuracy"])
+    gauge_precision.set(metrics["precision"])
+    gauge_recall.set(metrics["recall"])
+    gauge_f1.set(metrics["f1-score"])
+
+    print(f"Metrics logged to Prometheus")
+
+    return metrics
+
+# Set up the scheduler
+scheduler = BackgroundScheduler()
+scheduler.add_job(evaluate, 'interval', minutes=15)
+scheduler.start()
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Load the ML model
     ml_models["model"] = mlflow.sklearn.load_model('model')
-    #print(type(model))
     yield
     ml_models.clear()
+    scheduler.shutdown()
 
 app = FastAPI(lifespan=lifespan)
 
 class Iris(BaseModel):
-    data: List[List[float]] #Annotated[list[float], annotated_types.Len(min_length=4)]
+    data: List[List[float]]
 
 @app.get("/")
 def home():
-    return "Hello World"
+    return "Hello World..."
 
 
 @app.post('/predict', tags=["predictions"])
 def get_prediction(iris: Iris):
     data = dict(iris)['data']
-    model =  ml_models["model"]
+    model = get_model()
     prediction = model.predict(data).tolist()
     log_proba =  model.predict_proba(data).tolist()
     return {"prediction": prediction,
             "log_proba": log_proba}
 
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+def get_model():
+    model =  ml_models["model"]
+    return model
 
 
-Instrumentator().instrument(app).expose(app)
+@app.post('/evaluate')
+def call_evaluate():
+    return evaluate()
 
-Instrumentor.new_merit(){
+class Metrics(BaseModel):
+    accuracy: float
+    precision: float
+    recall: float
+    f1score: float
 
-    # connect to bd
+@app.post('/push-metrics')
+def push_metrics(metrics:Metrics):
+    gauge_acc.set(metrics.accuracy)
+    gauge_precision.set(metrics.precision)
+    gauge_recall.set(metrics.recall)
+    gauge_f1.set(metrics.f1score)
 
-    # calculer f1
-
-    # return f1-score
-    
-}
+instrumentator = Instrumentator(excluded_handlers=[".*admin.*", "/metrics"],)
+instrumentator.instrument(app).expose(app)
